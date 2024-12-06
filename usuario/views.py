caddf1458df756d4b1cf9e6bb445627f18f8ab15
Gabriel Sá <gabriel10043@live.com>
2024-12-06 18:13:48 -0300

    add signup

--- usuario/views.py
+++ usuario/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate
 from .serializers import UserLoginSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -23,3 +24,11 @@ class LoginView(APIView):
 				return Response({'token': token.key})
 			return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SignupView(APIView):
+	permission_classes = [AllowAny]
+	def post(self, request):
+		if (not "username" in request.data) or (not "email" in request.data) or (not "password" in request.data):
+			return Response({"detail": "Missing argument"}, status=status.HTTP_400_BAD_REQUEST)
+		user = User.objects.create_user(request.data["username"], request.data["email"], request.data["password"])
+		return Response({'detail': 'User created'})
